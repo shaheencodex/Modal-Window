e73f0c74dbb63694639402e66450195de5774a07
Shaheen Salma <shaheencodex@gmail.com>
2025-06-13 07:42:36 +0530

    eventlistneri is added

--- script.js
+++ script.js
@@ -24,6 +24,13 @@ btnClosedModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
 /*
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', function () {
